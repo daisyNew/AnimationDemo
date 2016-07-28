aec9a4d32ceb951bbf284c8bc5c064330acc9226
niugaifang <niugaifang@juesheng.com>
2016-07-28 10:20:10 +0800

    Initial Commit

---
+++ AnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTest
+//
+//  Created by DaisyNew on 16/7/28.
+//  Copyright © 2016年 DaisyNew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by DaisyNew on 16/7/28.
+//  Copyright © 2016年 DaisyNew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
